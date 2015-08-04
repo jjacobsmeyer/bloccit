74b9c59c6471b424a37d7a751226bffbb9d3abb2
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-08-04 15:13:39 -0500

    Assignment 47 completed, Destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,17 +18,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    # @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed"
-      redirect_to [@topic, @post]
+      redirect_to [@post]
     else
       flash[:error] = "Comment couldn't be deleted. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post]
     end
   end
 
