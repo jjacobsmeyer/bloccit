35746b2071ea9fd558807f86fff40e047e087d2b
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-08-07 18:40:47 -0500

    Checkpoint 50 incomplete, Mocking with RSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,8 +22,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: {minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-#   validates :topic, presence: true
-#   validates :user, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
 
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
@@ -32,10 +32,9 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
-  private
 
   def create_vote
-    user.votes.create(value: 1, post_id: id)
+    user.votes.create(value: 1, post: self)
   end
 
  end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Vote do
+include TestFactories
   describe "validations" do
     describe "value validation" do
       it "is valid with a value of '1'" do
@@ -25,4 +26,14 @@ describe Vote do
       end
     end
   end
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
+
 end
