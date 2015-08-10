1b508dcbcc04bceced42c6ed228abb781cdfa61a
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-08-10 10:36:50 -0500

    Checkpoint 50 completed, Mocking with RSpec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 describe VotesController do
 
+  include Devise::TestHelpers
   include TestFactories
 
   describe '#up_vote' do
     it "adds an up-vote to the post" do
       @user = authenticated_user
-      @post = associated post
+      @post = associated_post
       sign_in @user
+
+      request.env["HTTP_REFERER"] = '/'
 
       expect {
         post(:up_vote, post_id: @post.id)
