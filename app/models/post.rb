4be47a256540ddb72353c784115ac684de3d5bbd
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-08-18 14:32:14 -0500

    Checkpoint 54 completed, Public Profiles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-  has_many :favorites, dependent: :destroy 
+  has_many :favorites, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
@@ -20,6 +20,7 @@ class Post < ActiveRecord::Base
   end
 
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: {minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
