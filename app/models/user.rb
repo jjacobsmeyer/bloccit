b3be043afeb865f956f6e151c04163743932c38b
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-08-18 11:08:17 -0500

    Checkpoint 53 completed, Another Interlude

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ActiveRecord::Base
   def favorited(post)
     favorites.where(post_id: post.id).first
   end
-  
+
+  def voted(post)
+    votes.where(post_id: post.id).first
+  end
+
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,19 +8,39 @@ describe Comment do
 
     before do
       @post = associated_post
-      @user = authenticated_user(email_favorites: true)
-      @other_user = authenticeated_user
+      @user = authenticated_user
+      @other_user = authenticated_user
       @comment = Comment.new(body: 'My comment is really', post: @post, user: @other_user)
     end
 
+    context "with user's permission" do
+
     it "sends an email to users who have favorited  the post" do
-      favorite = @user.favorites.create(post: @post)
+       @user.favorites.where(post: @post).create
 
       allow(FavoriteMailer)
         .to receive(:new_comment)
+        .with(@user, @post, @comment)
+        .and_return(double(deliver_now: true))
+
+        expect(FavoriteMailer)
+          .to receive(:new_comment)
+
+        @comment.save
+      end
+    end
+
+    context "without permission" do
+      before { @user.update_attribute(:email_favorites, false) }
+
+      it "does not send emails, even to users who have favorited" do
+        @user.favorites.where(post: @post).create
+
+        expect( FavoriteMailer )
+        .not_to receive(:new_comment)
 
         @comment.save
       end
     end
   end
-  
+end
